eda074e2651f8a90864046a116e29376c66302a3
dashborr <danisarfas7@gmail.com>
2026-03-30 16:35:33 +0700

    Menambahkan input nilai Matematika dan Bahasa Inggris serta deklarasi variabel

--- Looping.cpp
+++ Looping.cpp
@@ -37,3 +37,13 @@ cout<<"PERULANGAN DO...WHILE"<<endl;
 
 #include <iostream>
 using namespace std;
+
+int main(){
+     float nilB, nilM, rerata;
+    string status;
+
+    cout<<"Masukkan nilai Matematika = ";
+    cin>>nilM;
+    cout<<"Masukkan nilai Bahasa Inggris = ";
+    cin>>nilB;
+}

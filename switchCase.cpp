7f20666389b472341991bd373af8d822da705e45
dashborr <danisarfas7@gmail.com>
2026-03-30 17:28:30 +0700

    menambahkan case 3

--- switchCase.cpp
+++ switchCase.cpp
@@ -26,6 +26,10 @@ void pilihanJurusan(){
             namaJurusan = "Teknik Elektro";
             statusKuota = "Kuota Penuh!";
             break;
+         case 3:
+            namaJurusan = "Sistem Informasi";
+            statusKuota = "Tersedia (5 Kursi)";
+            break;
      }
     
 }

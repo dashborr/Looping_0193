466557e8022b3cd50f109cc22d239d8772566f34
dashborr <danisarfas7@gmail.com>
2026-03-30 17:29:13 +0700

    menambahkan default case untuk error

--- switchCase.cpp
+++ switchCase.cpp
@@ -30,6 +30,9 @@ void pilihanJurusan(){
             namaJurusan = "Sistem Informasi";
             statusKuota = "Tersedia (5 Kursi)";
             break;
+        default:
+            namaJurusan = "Tidak Diketahui";
+            statusKuota = "Error: Kode Tidak Valid";
      }
     
 }

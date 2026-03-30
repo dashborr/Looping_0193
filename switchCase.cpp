f096e301cba7d38cff9eb4635bce416830c19f18
dashborr <danisarfas7@gmail.com>
2026-03-30 17:35:09 +0700

    menambahkan garis pemisah

--- switchCase.cpp
+++ switchCase.cpp
@@ -41,4 +41,5 @@ void output(){
      cout << "\n=== HASIL SELEKSI ===" << endl;
      cout << "Jurusan Pilihan : " << namaJurusan << endl;
      cout << "Status Kuota    : " << statusKuota << endl;
+      cout << "----------------------------------" << endl;
 }

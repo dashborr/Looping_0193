12de3847dc24feefe9ca95d58a2a2760edb25d5e
dashborr <danisarfas7@gmail.com>
2026-03-30 17:03:01 +0700

    Menambahkan perhitungan rata-rata, logika kelulusan menggunakan AND, dan output hasil

--- Looping.cpp
+++ Looping.cpp
@@ -75,4 +75,13 @@ cout<<"PERULANGAN DO...WHILE"<<endl;
     cin>>nilM;
     cout<<"Masukkan nilai Bahasa Inggris = ";
     cin>>nilB;
+
+     rerata = (nilB + nilM)/2;
+
+    if ((rerata >= 60) && (nilM >= 70))
+        status = "Lulus";
+    else
+        status = "Tidak lulus";
+
+    cout<<"Status kelulusan = "<<status<<", dengan nilai rata-rata = "<<rerata<<endl;
 }
